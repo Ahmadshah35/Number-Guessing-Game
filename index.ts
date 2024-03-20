226d25e42c72480988aca16ded5b634a53e071b8
Ahmadshah35 <155912240+Ahmadshah35@users.noreply.github.com>
2024-03-20 14:46:14 -0700

    my commit
    This is a Number Guessing Game.
    
    Initial commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+import inquirer from "inquirer"
+
+type ansType = {
+    userGuess: number
+}
+const systemGeneratedNo = Math.floor(Math.random() * 10);
+
+const answer: ansType = await inquirer.prompt([{
+    type:"number",
+    name:"userGuess",
+    message:"write your guess b/w  1 to 10"
+}
+])
+const {userGuess} = answer;
+
+    console.log(userGuess, "userGuess",systemGeneratedNo ,"Sys")
+    if( userGuess === systemGeneratedNo){
+    
+    console.log("Yaaa your answer is correct \n you win");
+    
+} else {
+    console.log("please try again better luck next time!");
+    
+}
